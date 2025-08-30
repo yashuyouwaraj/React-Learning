5c19753e55dce93ec483d7dcee1d4e82c4a258f0
Yashu Youwaraj <amanyasu@gmail.com>
2025-08-30 21:20:55 +0530

    01.Fundamentals/Styles done

---
+++ src/components/StyledCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StyledCart = () => {
+  return (
+    <div style={{ backgroundColor: "lightblue", padding: "8px", borderRadius: "10px", color: "darkblue"}}>
+        <h1>Style Cart</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod molestias necessitatibus odio, neque, dolores consequuntur aliquid numquam voluptas accusantium asperiores illo quis quibusdam! Accusantium explicabo ab repellendus ullam. Natus, est!</p>
+    </div>
+  )
+}
+
+export default StyledCart
