5d6a199c7a9100233dc2cc4eef84c4b355c442e2
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-12 06:44:29 +0530

    CleanUp

--- src/components/Add.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Add = () => {
-  return (
-    <div>something cool</div>
-  )
-}
-
-export default Add
--- src/components/Greet.jsx
+++
@@ -1,4 +0,0 @@
-const Greet = () => {
-    return <h1>Hello, User!</h1>;
-}
-export default Greet;
