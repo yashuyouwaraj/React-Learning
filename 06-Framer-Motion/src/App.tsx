f3b0d07726e94af05c5c5d0b2695ca3225028371
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-21 16:32:30 +0530

    Transitions

--- 06-Framer-Motion/src/App.tsx
+++ 06-Framer-Motion/src/App.tsx
@@ -1,14 +1,26 @@
 import { motion } from "motion/react"
 
+// 1. Transformations
+// const App = () => {
+//   return (
+//     <div>
+//       <div>
+//         <motion.div className="box" animate={{x:50 ,y:"-2rem"  }}/>
+//         <motion.div className="box" animate={{ rotate: 60, rotateX:80 }}/>
+//         <motion.div className="box" animate={{ translateY:150, scale:2, scaleX:2 }}/>
+//         <motion.div className="box" animate={{ y:300, skewX:20, skewY:20 }}/>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+// 2. Transitions
+
 const App = () => {
   return (
     <div>
-      <div>
-        <motion.div className="box" animate={{x:50 ,y:"-2rem"  }}/>
-        <motion.div className="box" animate={{ rotate: 60, rotateX:80 }}/>
-        <motion.div className="box" animate={{ translateY:150, scale:2, scaleX:2 }}/>
-        <motion.div className="box" animate={{ y:300, skewX:20, skewY:20 }}/>
-      </div>
+      <motion.div className="box" initial={{x:-300}} animate={{x:300}} transition={{duration: 3 , delay:3 , ease: "backInOut"}} />
     </div>
   )
 }
