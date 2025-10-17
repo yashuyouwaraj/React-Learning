f7039ec8d2c5ff0910026cbba3e37a32281031cd
Yashu Youwaraj <amanyasu@gmail.com>
2025-10-17 11:05:40 +0530

    Default Slots

--- 10-React-Design-Patterns/8.Slots/Default Slots/src/App.tsx
+++ 10-React-Design-Patterns/8.Slots/Default Slots/src/App.tsx
@@ -1,8 +1,16 @@
+import Card from "./components/Card"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Card>
+        {/* Default Slots (Anonymous Slots) */}
+        <h1>This is my Card Title</h1>
+        <p>This is my card content</p>
+        <button>Learn More</button>
+      </Card>
+    </div>
   )
 }
 
---
+++ 10-React-Design-Patterns/8.Slots/Default Slots/src/components/Card.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react"
+
+const Card = ({children}:{children:ReactNode}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Card
