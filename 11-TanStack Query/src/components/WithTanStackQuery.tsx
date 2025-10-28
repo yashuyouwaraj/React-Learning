6772579c18664c57b8a490953f8314b35c3ffb19
Yashu Youwaraj <amanyasu@gmail.com>
2025-10-28 19:56:03 +0530

    WithTanStack Query

--- 11-TanStack Query/src/components/WithTanStackQuery.tsx
+++ 11-TanStack Query/src/components/WithTanStackQuery.tsx
@@ -1,8 +1,35 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+//With Fetch Method 👇
+// const fetchData = async ()=>{
+//     const response= await fetch("https://jsonplaceholder.typicode.com/todos")
+//     if(!response.ok) throw new Error("Network Response was not ok")
+//     return response.json()
+// }
+
+// Fetching Data with Axios 👇
+
+const fetchData = async ()=>{
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    return response.data
+}
+
 
 const WithTanStackQuery = () => {
+
+    const {data,error,isLoading}=useQuery({
+        queryKey:["todo"],
+        queryFn:fetchData,
+    })
+
+    if(isLoading) return <div>Loading....</div>
+    if(error) return <div>An error occurred: {error.message}</div>
+
   return (
     <div>
-        
+        <h1>Data</h1>
+        <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   )
 }
