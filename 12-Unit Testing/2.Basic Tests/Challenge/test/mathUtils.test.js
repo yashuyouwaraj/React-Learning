a26e0e1cc979985d7f275987ef84bb7b2a1538b2
Yashu Youwaraj <amanyasu@gmail.com>
2025-10-30 10:18:22 +0530

    fix

--- 12-Unit Testing/2.Basic Tests/Challenge/test/mathUtils.test.js
+++ 12-Unit Testing/2.Basic Tests/Challenge/test/mathUtils.test.js
@@ -1,4 +1,4 @@
-import {describe,it,test,expect} from "vitest"
+import {describe,it,expect} from "vitest"
 import { factorial, fibonacci, gcd } from "../src/mathUtils"
 
 describe("Math Utils",()=>{
