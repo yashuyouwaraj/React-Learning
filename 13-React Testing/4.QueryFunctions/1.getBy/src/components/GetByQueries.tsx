652d4826eeefb62628d186a05fe3499c7aaf5220
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-05 11:41:30 +0530

    React Testing - QueryFunctions- getBy

---
+++ 13-React Testing/4.QueryFunctions/1.getBy/src/components/GetByQueries.tsx
@@ -0,0 +1,15 @@
+
+const GetByQueries = () => {
+  return (
+    <div>
+      <h1>MyComponent</h1>
+      <button aria-label="submit">Submit</button>
+      <button id="cancel-button">Cancel</button>
+      <input type="text" placeholder="Enter text" />
+      <a href="https://example.com" target="_black" rel="noopener noreferrer">Visit Example</a>
+      <div data-testid="custom-element">Custom Element</div>
+    </div>
+  )
+}
+
+export default GetByQueries
---
+++ 13-React Testing/4.QueryFunctions/5.findBy/src/components/UserStatus.tsx
@@ -0,0 +1,8 @@
+
+const UserStatus = ({ email }:{ email:string }) => {
+  return (
+    <div>{ email? <h1>Welcome {email}</h1>: <button>Sign Up</button> }</div>
+  )
+}
+
+export default UserStatus
