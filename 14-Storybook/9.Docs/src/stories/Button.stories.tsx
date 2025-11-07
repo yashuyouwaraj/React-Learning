2b95518ddc5c6dca89cf5d69f6a1f3577f893135
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-07 11:38:23 +0530

    Storybook - Storybook with TypeScript and Docs

---
+++ 14-Storybook/9.Docs/src/stories/Button.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {Button,type ButtonProps} from "../components/Button"
+
+const meta:Meta<ButtonProps>={
+  title:"Components/Button",
+  component:Button,
+  tags:["autodocs"],
+  argTypes:{
+    color:{control:"color"},
+    label:{control:"text"},
+    disabled:{control:"boolean"}
+  }
+}
+
+export default meta;
+
+export const Default:StoryObj<ButtonProps>={
+  args:{
+    label:"Click Me",
+    color:"#007bff",
+    disabled:false,
+  }
+}
+
+export const Disabled:StoryObj<ButtonProps>={
+  args:{
+    label:"Click Me",
+    color:"#888",
+    disabled:true,
+  }
+}
+
+export const Red:StoryObj<ButtonProps>={
+  args:{
+    label:"Click Me",
+    color:"#ff0000",
+    disabled:false,
+  }
+}
