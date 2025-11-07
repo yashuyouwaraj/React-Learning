9c01df0086f6b0e52edaf968b6a7db12cc8cbcc0
Yashu Youwaraj <amanyasu@gmail.com>
2025-11-07 10:36:44 +0530

    Storybook - Parameters

---
+++ 14-Storybook/6.Parameters/.storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from '@storybook/react-vite'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#333333" },
+      ],
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+};
+
+export default preview;
